845507a3b6f1a04fb90b916533abd6fb4fb85851
a13400880 <Qwerty@1234>
2023-07-18 21:38:00 +0530

    committing for ValidAnagram.

---
+++ src/main/java/strings/ValidAnagram.java
@@ -0,0 +1,33 @@
+package src.main.java.strings;
+
+public class ValidAnagram {
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+
+        System.out.println(isAnagram(s, t));
+    }
+
+    private static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] count = new int[26];
+        boolean isAnagram = true;
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                isAnagram = false;
+                break;
+            }
+        }
+
+        return isAnagram;
+    }
+}
