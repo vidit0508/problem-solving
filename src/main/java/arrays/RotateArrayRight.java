b5870a397079ce158b0c4a8566f057bbca1ca975
a13400880 <Qwerty@1234>
2023-07-22 13:01:45 +0530

    committing for RotateArrayRight.

---
+++ src/main/java/arrays/RotateArrayRight.java
@@ -0,0 +1,36 @@
+package src.main.java.arrays;
+
+public class RotateArrayRight {
+
+    public static void main(String[] args) {
+        int[] nums = {-1,-100,3,99};
+        int k = 2;
+        print(nums);
+        rotateRight(nums, k);
+        print(nums);
+    }
+
+    private static void rotateRight(int[] nums, int k) {
+        k = k % nums.length;
+        rotate(nums, 0, nums.length - 1);
+        rotate(nums, 0, k - 1);
+        rotate(nums, k, nums.length - 1);
+    }
+
+    private static void rotate(int[] nums, int left, int right) {
+        while (left <= right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    private static void print(int[] nums) {
+        System.out.println();
+        for (int i : nums) {
+            System.out.print(i + " ");
+        }
+    }
+}
