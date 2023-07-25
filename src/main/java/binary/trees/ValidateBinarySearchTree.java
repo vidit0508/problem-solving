b01946493fe21fe34e4778bd9d31e7ee45f48792
a13400880 <Qwerty@1234>
2023-07-25 10:20:42 +0530

    committing for ValidateBinarySearchTree.

---
+++ src/main/java/binary/trees/ValidateBinarySearchTree.java
@@ -0,0 +1,31 @@
+package src.main.java.binary.trees;
+
+public class ValidateBinarySearchTree {
+
+    public static void main(String[] args) {
+        isValidBST(new TreeNode());
+    }
+
+    public static boolean isValidBST(TreeNode root) {
+        if (null == root) {
+            return true;
+        }
+        return isValid(root.left, null, root.val)
+                && isValid(root.right, root.val, null);
+    }
+
+    private static boolean isValid(TreeNode node, Integer min, Integer max) {
+        if (null == node) {
+            return true;
+        }
+
+        if ((null != max && node.val >= max)
+                || (null != min && node.val <= min)) {
+            return false;
+        }
+
+        return isValid(node.left, min, node.val)
+                && isValid(node.right, node.val, max);
+    }
+
+}
