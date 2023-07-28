2768c8d799eb547fceec17b0fdec1fe344f59d5c
a13400880 <Qwerty@1234>
2023-07-28 23:14:47 +0530

    committing for CombinationSum.

---
+++ src/main/java/arrays/CombinationSum.java
@@ -0,0 +1,45 @@
+package src.main.java.arrays;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CombinationSum {
+
+    static List<List<Integer>> res = new ArrayList<>();
+
+    public static void main(String[] args) {
+        int[] candidates = {2,3,6,7};
+        int target = 7;
+        System.out.println(combinationSum(candidates, target));
+    }
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<Integer> list = new ArrayList<>();
+        getSums(candidates, target, 0, 0, list);
+        return res;
+    }
+
+    public static void getSums(int[] candidates, int target, int index, int currSum, List<Integer> list) {
+        // bounding condition
+        if (currSum > target) {
+            return;
+        }
+
+        // base case
+        if (index == candidates.length) {
+            if (currSum == target) {
+                res.add(new ArrayList<>(list));
+            }
+            return;
+        }
+
+        currSum = currSum + candidates[index];
+        list.add(candidates[index]);
+        getSums(candidates, target, index, currSum, list);
+        currSum = currSum - candidates[index];
+        list.remove(list.size() - 1);
+
+        getSums(candidates, target, index + 1, currSum, list);
+    }
+}
