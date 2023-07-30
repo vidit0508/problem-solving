79eae81a90f1831cebb0e4e4345c36760f75f933
a13400880 <Qwerty@1234>
2023-07-30 23:43:33 +0530

    committing for NumberOfIslands.

--- src/main/java/graph/NumberOfIslands.java
+++ src/main/java/graph/NumberOfIslands.java
@@ -3,6 +3,7 @@ package src.main.java.graph;
 public class NumberOfIslands {
 
     public static void main(String[] args) {
+
         char[][] grid = {{'1','1','1','1','0'},
                 {'1','1','0','1','0'},
                 {'1','1','0','0','0'},
