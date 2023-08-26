ebde33543d29345a3e0989da2548c31e1ec9d754
a13400880 <Qwerty@1234>
2023-08-26 16:15:19 +0530

    committing for FindMinimumInRotatedSortedArray.

---
+++ src/main/java/searchingandsorting/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,24 @@
+package src.main.java.searchingandsorting;
+
+public class FindMinimumInRotatedSortedArray {
+
+    public static void main(String[] args) {
+        int[] nums = {3,4,5,1,2};
+        System.out.println(getSmalletElement(nums));
+    }
+
+    private static int getSmalletElement(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] < nums[end]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return nums[start];
+    }
+}
