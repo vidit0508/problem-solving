011107db5ef554f5957c29e7724008e0df01538b
a13400880 <Qwerty@1234>
2023-09-23 12:36:03 +0530

    committing for MaximumSubarray.

--- src/main/java/arrays/MaximumSubarray.java
+++ src/main/java/arrays/MaximumSubarray.java
@@ -5,6 +5,36 @@ public class MaximumSubarray {
     public static void main(String[] args) {
         int[] nums = {5,4,-1,7,8};
         System.out.println(maxSubArray(nums));
+        printMaximumSumSubarray(nums);
+    }
+
+    // by this way we can print sub array with maximum sum
+    private static void printMaximumSumSubarray(int[] nums) {
+        int currSum = nums[0];
+        int maxSum = nums[0];
+        int startIndex = 0;
+        int endIndex = 0;
+        int maxStartIndex = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i] + currSum) {
+                currSum = nums[i];
+                startIndex = i;
+            } else if (nums[i] < nums[i] + currSum) {
+                currSum = currSum + nums[i];
+            }
+
+            if (currSum > maxSum) {
+                maxSum = currSum;
+                endIndex = i;
+                maxStartIndex = startIndex;
+            }
+        }
+
+        for (int i = maxStartIndex; i <= endIndex; i++) {
+            System.out.print(nums[i] + " ");
+        }
+
     }
 
     private static int maxSubArray(int[] nums) {
