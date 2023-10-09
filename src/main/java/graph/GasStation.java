0b11242527d841542608789dc7021e12fb2abb7b
a13400880 <Qwerty@1234>
2023-10-09 21:59:12 +0530

    committing for GasStation.

---
+++ src/main/java/graph/GasStation.java
@@ -0,0 +1,45 @@
+package src.main.java.graph;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class GasStation {
+
+    public static void main(String[] args) {
+        int[] gas = {1, 2, 3, 4, 5};
+        int[] cost = {3, 4, 5, 1, 2};
+
+        System.out.println(canCompleteCircuit(gas, cost));
+    }
+
+    public static int canCompleteCircuit(int[] gas, int[] cost) {
+        int gasSum = getSum(gas);
+        int costSum = getSum(cost);
+
+        // below condition means that solution is not present
+        if (gasSum < costSum) {
+            return -1;
+        }
+
+        int res = 0;
+        int sum = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            sum = sum + (gas[i] - cost[i]);
+            if (sum < 0) {
+                sum = 0;
+                // we are adding plus one directly because we are sure from sum logic then solution is present
+                res = i + 1;
+            }
+        }
+        return res;
+    }
+
+    private static int getSum(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+        }
+        return sum;
+    }
+}
