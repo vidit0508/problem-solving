d605b1b7719bd712785b916cf47f4c678c2f6720
a13400880 <Qwerty@1234>
2023-10-14 13:31:30 +0530

    committing for IsomorphicStrings.

---
+++ src/main/java/companywise/linkedin/easy/IsomorphicStrings.java
@@ -0,0 +1,27 @@
+package src.main.java.companywise.linkedin.easy;
+
+public class IsomorphicStrings {
+
+    public static void main(String[] args) {
+        String s = "paper";
+        String t = "title";
+        System.out.println(isIsomorphic(s, t));
+    }
+
+    public static boolean isIsomorphic(String s, String t) {
+
+        int[] sMap = new int[256];
+        int[] tMap = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+            if (sMap[s.charAt(i)] != tMap[t.charAt(i)]) {
+                return false;
+            }
+
+            sMap[s.charAt(i)] = i + 1;
+            tMap[t.charAt(i)] = i + 1;
+        }
+
+        return true;
+    }
+}
