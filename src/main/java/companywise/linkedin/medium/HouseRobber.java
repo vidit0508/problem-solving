55cdeb0b61eb9a36e3ce471dda362f2f5ccefcfd
a13400880 <Qwerty@1234>
2023-10-21 20:28:12 +0530

    committing for HouseRobber

---
+++ src/main/java/companywise/linkedin/medium/HouseRobber.java
@@ -0,0 +1,31 @@
+package src.main.java.companywise.linkedin.medium;
+
+public class HouseRobber {
+
+    public static void main(String[] args) {
+        int[] nums = {2,7,9,3,1};
+        System.out.println(rob(nums));
+    }
+
+    public static int rob(int[] nums) {
+
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int[] ans = new int[nums.length];
+        ans[0] = nums[0];
+        ans[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            ans[i] = Math.max(ans[i - 1], nums[i] + ans[i - 2]);
+        }
+
+        return ans[nums.length - 1];
+    }
+
+}
