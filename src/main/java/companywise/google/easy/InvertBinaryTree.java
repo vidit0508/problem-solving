904dbc27dc11aa4097b10a95072e7399d59baa6a
a13400880 <Qwerty@1234>
2023-12-10 13:37:43 +0530

    committing for InvertBinaryTree.java

---
+++ src/main/java/companywise/google/easy/InvertBinaryTree.java
@@ -0,0 +1,20 @@
+package src.main.java.companywise.google.easy;
+
+import src.main.java.topicwise.binary.trees.TreeNode;
+
+public class InvertBinaryTree {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+}
