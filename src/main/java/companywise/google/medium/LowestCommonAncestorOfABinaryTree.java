37add3dd501c6c693b4a002f831558fb83705988
a13400880 <Qwerty@1234>
2023-12-20 13:58:01 +0530

    committing for LowestCommonAncestorOfABinaryTree.java

---
+++ src/main/java/companywise/google/medium/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,29 @@
+package src.main.java.companywise.google.medium;
+
+import src.main.java.topicwise.binary.trees.TreeNode;
+
+public class LowestCommonAncestorOfABinaryTree {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return root;
+        }
+
+        if (root.val == p. val || root.val == q.val) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left == null) {
+            return right;
+        }
+
+        if (right == null) {
+            return left;
+        }
+
+        return root;
+    }
+}
