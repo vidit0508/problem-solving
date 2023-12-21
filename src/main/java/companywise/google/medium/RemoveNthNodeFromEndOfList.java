0142809264c5b304be3ece7e6106e6f94e89856e
a13400880 <Qwerty@1234>
2023-12-21 14:49:20 +0530

    committing for RemoveNthNodeFromEndOfList.java

---
+++ src/main/java/companywise/google/medium/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,29 @@
+package src.main.java.companywise.google.medium;
+
+import src.main.java.topicwise.linked.list.ListNode;
+
+public class RemoveNthNodeFromEndOfList {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        if (fast == null) {
+            head = head.next;
+            return head;
+        }
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return head;
+    }
+}
