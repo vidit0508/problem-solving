43a9f87dc59dfa40870b9cae2cf6623514dd9f41
a13400880 <Qwerty@1234>
2024-01-20 11:47:55 +0530

    committing for JumpGameII.java

---
+++ src/main/java/top/onefifty/medium/JumpGameII.java
@@ -0,0 +1,26 @@
+package src.main.java.top.onefifty.medium;
+
+public class JumpGameII {
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,1,1,4};
+        System.out.println(jump(nums));
+    }
+
+    public static int jump(int[] nums) {
+        int start = 0;
+        int end = 0;
+        int jumpCount = 0;
+
+        while (end < nums.length - 1) {
+            int farthest = 0;
+            for (int i = start; i <= end; i++) {
+                farthest = Math.max(farthest, i + nums[i]);
+            }
+            start = end + 1;
+            end = farthest;
+            jumpCount++;
+        }
+        return jumpCount;
+    }
+}
