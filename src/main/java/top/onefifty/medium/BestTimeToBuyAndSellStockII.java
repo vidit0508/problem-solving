5fe5c1e17ef3eac3ecffca1c8a9ff75a549da16b
a13400880 <Qwerty@1234>
2024-01-20 10:41:38 +0530

    committing for BestTimeToBuyAndSellStockII.java

---
+++ src/main/java/top/onefifty/medium/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,24 @@
+package src.main.java.top.onefifty.medium;
+
+public class BestTimeToBuyAndSellStockII {
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minIndex = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < prices[minIndex]) {
+                minIndex = i;
+            } else {
+                maxProfit = maxProfit + (prices[i] - prices[minIndex]);
+                minIndex = i;
+            }
+        }
+        return maxProfit;
+    }
+}
