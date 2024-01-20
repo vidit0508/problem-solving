f2d8c9b96fe4c57a714cd662b5ed03d7fc605f95
a13400880 <Qwerty@1234>
2024-01-20 11:53:52 +0530

    committing for HIndex.java

---
+++ src/main/java/top/onefifty/medium/HIndex.java
@@ -0,0 +1,23 @@
+package src.main.java.top.onefifty.medium;
+
+import java.util.Arrays;
+
+public class HIndex {
+
+    public static void main(String[] args) {
+        int[] citations = {3,0,6,1,5};
+        System.out.println(hIndex(citations));
+    }
+
+    private static int hIndex(int[] citations) {
+        int len = citations.length;
+        int i = 0;
+
+        Arrays.sort(citations);
+
+        while (i < len && citations[len - 1 - i] > i) {
+            i++;
+        }
+        return i;
+    }
+}
