6b65fe4cde566f670599e0d2c47394e56b2a8921
a13400880 <Qwerty@1234>
2024-03-05 23:32:42 +0530

    committing for LongestPalindromicSubsequence.java

---
+++ src/main/java/companywise/expedia/LongestPalindromicSubsequence.java
@@ -0,0 +1,37 @@
+package src.main.java.companywise.expedia;
+
+public class LongestPalindromicSubsequence {
+
+    public static void main(String[] args) {
+        String s = "bbbab";
+        System.out.println(longestPalindromeSubseq(s));
+    }
+
+    public static int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] memo = new int[n][n];
+        return lps(s, 0, n - 1, memo);
+    }
+
+    private static int lps(String s, int i, int j, int[][] memo) {
+        if (memo[i][j] != 0) {
+            return memo[i][j];
+        }
+
+        if (i > j) {
+            return 0;
+        }
+
+        if (i == j) {
+            return 1;
+        }
+
+        if (s.charAt(i) == s.charAt(j)) {
+            memo[i][j] = lps(s, i + 1, j - 1, memo) + 2;
+        } else {
+            memo[i][j] = Math.max(lps(s, i + 1, j, memo), lps(s, i, j - 1, memo));
+        }
+
+        return memo[i][j];
+    }
+}
